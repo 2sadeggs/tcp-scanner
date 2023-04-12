54644006aa16fcf3c5a870c4c898a8e930b54091
Mario <hi163163@126.com>
2023-04-12 10:27:21 +0800

    V2-添加for循环 遍历更多端口

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"net"
-)
-
 /*https://www.infoq.cn/article/YfRpv2IpRNh2WXDF6PDv?source=app_share*/
 //我们 TCP 扫描器第一步先实现单个端口的测试。使用标准库中的 net.Dial 函数，该函数接收两个参数：协议和测试地址（带端口号）。
+func isOpen() {
+
+}
 
 func main() {
+
+}
+
+/*func main() {
 	//_, err := net.Dial("tcp", "www.baidu.com:80")
 	_, err := net.Dial("tcp", "google.com:80")
 	if err == nil {
@@ -16,9 +18,22 @@ func main() {
 	} else {
 		fmt.Println(err)
 	}
-}
-
+}*/
 /*执行时间很长 卡死在等待页面 需要处理超时*/
 /*PS D:\GolandProjects\tcp-scanner> go run .\main.go
 dial tcp 142.251.43.14:80: connectex: A connection attempt failed because the connected party did not properly respond after a period of time, or establ
 ished connection failed because connected host has failed to respond.*/
+
+/*func main() {
+	for port := 80; port < 100; port++ {
+		conn, err := net.Dial("tcp", fmt.Sprintf("google.com:%d", port))
+		if err == nil {
+			conn.Close()
+			fmt.Println("Connection successful!")
+		} else {
+			fmt.Println(err)
+		}
+	}
+}*/
+
+/*每个都超时 加上for循环更多超时*/
